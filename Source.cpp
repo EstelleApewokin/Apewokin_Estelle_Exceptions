111b563d3d69dc75da52b58a64a623653a9d9b02
Estelle Apewokin <estelliosis@icloud.com>
2022-07-31 23:18:27 -0400

    created function, working on try/catch construct

--- Source.cpp
+++ Source.cpp
@@ -6,9 +6,41 @@
 #include <iostream>
 #include <iomanip>
 
+using namespace std;
+
+char character(char, int);
+
+
 int main() {
+	char input = 'C';
+	int offset = 3;
+	
+	try {
+		cout << character(input, offset);
+	}
+	catch (string) {
 
-
-
+	}
 	return 0;
+
 }
+
+char character(char start, int offset) {
+	char end;
+
+	if (!isalpha(start)) {
+		string invalidCharacterException = "ERROR: starting letter is not a letter A-Z.\n";
+		throw invalidCharacterException;
+	}
+	
+	end = start + offset;
+
+	if (!isalpha(end)) {
+		string invalidRangeException = "ERROR: target letter is not a letter A-Z.\n";
+		throw invalidRangeException;
+	}
+
+	return end;
+
+}
+
